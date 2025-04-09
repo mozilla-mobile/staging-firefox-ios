60cc04277e4921065ed4e3a6e1476f346d60e7b0
Bastien Orivel <borivel@mozilla.com>
2025-04-09 13:55:54 +0200

    Fix TODO, we don't use build-type

--- taskcluster/ffios_taskgraph/target_tasks.py
+++ taskcluster/ffios_taskgraph/target_tasks.py
@@ -65,11 +65,7 @@ def target_tasks_ship(full_task_graph, parameters, graph_config):
     )
 
 def does_task_match_release_type(task, release_type):
-    return (
-        # TODO: only use a single attribute to compare to `release_type`
-        task.attributes.get("build-type") == release_type
-        or task.attributes.get("release-type") == release_type
-    )
+    return task.attributes.get("release-type") == release_type
 
 def _filter_release_promotion(
     full_task_graph, parameters, filtered_for_candidates, shipping_phase

82ab34321592c813054b479960c3b94b9941717d
Bastien Orivel <borivel@mozilla.com>
2025-07-09 10:42:48 +0200

    WIP

--- taskcluster/ffios_taskgraph/transforms/relpro_push.py
+++ taskcluster/ffios_taskgraph/transforms/relpro_push.py
@@ -7,11 +7,16 @@ transforms = TransformSequence()
 @transforms.add
 def resolve_keyed_by_build_type(config, tasks):
     for task in tasks:
-        resolve_keyed_by(task, 'treeherder.symbol', task["name"], **{'build-type': task['attributes']['build-type']})
+        resolve_keyed_by(task, 'treeherder.symbol', task["name"], **{'build-type': task['attributes']['build-type'], 'flavor': task['attributes'].get('flavor')})
         build_type = task["attributes"]["build-type"]
+        is_focus = task["attributes"]["product-type"] == "focus"
+        dependency_label = task["attributes"]["flavor"] if is_focus else build_type
 
         for workflow in task["worker"]["bitrise"]["workflows"]:
             for params in workflow.values():
-                params.append({"BUILD_TASK_ID": {"task-reference": f"<promote-{build_type}>"}})
+                param = {"BUILD_TASK_ID": {"task-reference": f"<promote-{dependency_label}>"}}
+                if is_focus:
+                    param["API_BITRISE_SCHEME"] = task["attributes"]["flavor"].capitalize()
+                params.append(param)
 
         yield task
